718315291da5a89bef00611e5428949518311420
justin honey <jhoney92021@gmail.com>
2022-09-07 15:27:49 -0600

    wip

--- IntermediateObjectDefinitions/SuiteAndName.cs
+++ IntermediateObjectDefinitions/SuiteAndName.cs
@@ -1,7 +1,9 @@
+using JaySharp.TestAttributes;
 namespace JaySharp.IntermediateObjectDefinitions;
 
 public struct SuiteAndName
 {
     public Type Type;
+    public JayTestSuite Suite;
     public string Name;
 }
--- TestRunner/TestRunnerPrivateMethods.cs
+++ TestRunner/TestRunnerPrivateMethods.cs
@@ -20,9 +20,28 @@ public static partial class TestRunner
     {
         if(TestSuitesToRun != null)
         {
+            Type currentSuite;
+            int idx = 0;
             TestsToRun = new List<MethodAndSuiteName>();
             foreach(var suite in TestSuitesToRun)
             {
+
+                currentSuite = TestSuitesToRun[idx].Type;
+JayLogger.PrintWithColor($"~~ base {TestSuitesToRun[idx].Type.Attributes} ~~", ConsoleColor.Blue);
+JayLogger.PrintWithColor($"~~ declare {TestSuitesToRun[idx].Type.GetCustomAttributesData()[0]} ~~", ConsoleColor.Blue);
+JayLogger.PrintWithColor($"~~ declare {TestSuitesToRun[idx].Type.GetCustomAttributesData()[0].AttributeType} ~~", ConsoleColor.Green);
+var ff = TestSuitesToRun[idx].Type.GetCustomAttributesData();
+JayLogger.PrintWithColor($"~~ ff {ff.FirstOrDefault()} ~~", ConsoleColor.Green);
+var fsf = ff.SelectMany(anon => anon.NamedArguments).Where(anon => anon.MemberName == "Off");
+JayLogger.PrintWithColor($"~~ fsf {fsf.Count()} ~~", ConsoleColor.Yellow);
+JayLogger.PrintWithColor($"~~ fsf {fsf.FirstOrDefault()} ~~", ConsoleColor.Green);
+var bb = fsf.Select(na => na.TypedValue.Value);
+JayLogger.PrintWithColor($"~~ bb {bb.FirstOrDefault()} ~~", ConsoleColor.Green);
+JayLogger.PrintWithColor($"~~ decasdasdaslare {TestSuitesToRun[idx].Type.GetCustomAttribute(typeof(JayTestSuite))} ~~", ConsoleColor.Blue);
+JayLogger.PrintWithColor($"~~ decasdasdaslare {TestSuitesToRun[idx].Type.GetCustomAttributesData().Select(anon => anon.NamedArguments[0].TypedValue.Value)} ~~", ConsoleColor.Red);
+JayLogger.PrintWithColor($"~~ decasdasdaslare {TestSuitesToRun[idx].Type.GetCustomAttributesData().Select(anon => anon.NamedArguments[0].MemberInfo.Name)} ~~", ConsoleColor.Red);
+                idx++;
+JayLogger.PrintWithColor($"~~ is type of {currentSuite} ~~", ConsoleColor.Blue);
                 TestsToRun.AddRange(
                     GetMethodsWithAttribute(suite, TestType)
                 );                
@@ -35,11 +54,12 @@ public static partial class TestRunner
         if(TestsToRun != null)
         {
             string currentSuiteName = string.Empty;
+
             foreach(var methodAndSuiteName in TestsToRun)
             {
                 if(currentSuiteName != methodAndSuiteName.SuiteName || string.IsNullOrEmpty(currentSuiteName))
                 {
-                    currentSuiteName= methodAndSuiteName.SuiteName;
+                    currentSuiteName = methodAndSuiteName.SuiteName;
                     JayLogger.PrintWithColor($"~~ Running {currentSuiteName} ~~", ConsoleColor.Blue);
                 }
                 var method = methodAndSuiteName.Method;
@@ -65,7 +85,7 @@ public static partial class TestRunner
         return assembly
                 .GetTypes()
                 .Where(type => type.GetCustomAttributes(attribute, true).Length > 0)
-                .Select(type => new SuiteAndName{Type = type, Name = type.Name  })
+                .Select(type => new SuiteAndName{Type = type, Name = type.Name })
                 .ToArray();
     }
 
